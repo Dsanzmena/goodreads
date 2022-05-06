2756a44949673c426ecad8ed518de1ee7cd0836a
Dsanzmena <56298642+Dsanzmena@users.noreply.github.com>
2022-05-06 21:37:42 +0200

    Second commit 06/05/2022

--- goodreads/src/main/java/com/dsanz/goodreads/goodreads/controller/HomeController.java
+++ goodreads/src/main/java/com/dsanz/goodreads/goodreads/controller/HomeController.java
@@ -1,5 +1,23 @@
 package com.dsanz.goodreads.goodreads.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
 public class HomeController {
 
+	private static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = { "/", "home" })
+	public ModelAndView loadHome(HttpServletRequest request) {
+		logger.debug(" >>> HomeController GET --> home.html ");
+		ModelAndView mav = new ModelAndView("home");
+		logger.debug(" >>> HomeController RETURNED --> OK! ");
+		return mav;
+	}
 }
